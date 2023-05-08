5b72e206ba5cdfcab52fac771a52cdd894f52658
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-05-08 15:59:24 -0300

    Creating the decision structure to filter user input

--- question1/question1.java
+++ question1/question1.java
@@ -7,7 +7,7 @@ public class question1 {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("####### QUESTION 1 #######");
+        System.out.println("\n####### QUESTION 1 #######");
         System.out.println("Java program that aims to validate whether a given employee is entitled to a bonus.");
 
         int option;
@@ -22,7 +22,24 @@ public class question1 {
 
             switch (option) {
                 case 1:
-                    System.out.println("Opção 1");
+                    System.out.print("Por favor, digite o número de funcionários que deseja cadastrar: ");
+                    int numberOfEmployees = scan.nextInt();
+
+                    if (numberOfEmployees < 0) {
+                        System.out.println("\n****************************************************************\n");
+                        System.out.println("Parece que você digitou um número negativo, vamos recomeçar!");
+                        System.out.println("Para o programa realmente funcionar digite um número maior que 0!");
+                        System.out.println("\n****************************************************************\n");
+                    } else if (numberOfEmployees == 0) {
+                        System.out.println("\n****************************************************************\n");
+                        System.out.println("Parece que você digitou zero e não quer mais cadastrar funcionários");
+                        System.out.println("Vamos voltar ao nosso menu então!");
+                        System.out.println("\n****************************************************************\n");
+                    } else {
+                        System.out.println("\n****************************************************************\n");
+                        System.out.println("Excelente vamos cadastrar esses " + numberOfEmployees + " funcionários");
+                        System.out.println("\n****************************************************************\n");
+                    }
                     break;
                 case 2:
                     System.out.println("\nSaindo do programa...");
@@ -31,6 +48,5 @@ public class question1 {
                     System.out.println("\n****************** \nOpção inválida!!!\n******************");
             }
         } while (option != 2);
-
     }
 }
