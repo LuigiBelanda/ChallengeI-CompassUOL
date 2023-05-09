f21566c5374048f10586ee97625b6ed5e70bcbe8
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-05-09 16:36:47 -0300

    Modify name of the fields

--- question4/question4.sql
+++ question4/question4.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE question4 
 
 CREATE TABLE Address (
-	addressId 		INT UNIQUE PRIMARY KEY,
-	zip 			VARCHAR[10],
-	street 			VARCHAR[100],
+	address_id 		INT 			UNIQUE PRIMARY KEY,
+	zip_code		VARCHAR[10],
+	address 		VARCHAR[100],
 	complement 		VARCHAR[100],
 	neighborhood 	VARCHAR[100],
 	city 			VARCHAR[100],
@@ -12,15 +12,15 @@ CREATE TABLE Address (
 );
 
 CREATE TABLE Person (
-	person_id 		INT UNIQUE PRIMARY KEY,
-	name 			VARCHAR[255],
-	age 			SMALLINT,
-	phone 			VARCHAR[15],
-	height 			FLOAT, 
-	email 			VARCHAR[255],
-	cpf 			VARCHAR[14],
-	bithday 		DATE,
-	address_id 		INT, 
+	person_id 			INT 			UNIQUE PRIMARY KEY,
+	name 				VARCHAR[255],
+	age 				SMALLINT,
+	phone 				VARCHAR[15],
+	height 				FLOAT, 
+	email 				VARCHAR[255],
+	CPF 				VARCHAR[14],
+	date_of_bithday 	DATE,
+	address_id 			INT, 
 	
 	FOREIGN KEY (address_id) REFERENCES Address (addressId),
 	CHECK (age > 0)
