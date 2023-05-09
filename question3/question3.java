d9bc6afdf873ae80201d5fda44b54fbcfc223270
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-05-09 13:44:09 -0300

    Adding user response filtering and adding hit and miss counting logic

--- question3/question3.java
+++ question3/question3.java
@@ -6,7 +6,7 @@ public class question3 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("\n####### QUESTION 2 #######");
+        System.out.println("\n####### QUESTION 3 #######");
 
         int option;
 
@@ -23,6 +23,13 @@ public class question3 {
                     String nameUser;
                     int rights = 0, wrongs = 0;
 
+                    String[][] arrayOfQuestionsAndAnswers = {
+                        { "Questão 1 - AA", "V" },
+                        { "Questão 1 - BB", "FLW" },
+                        { "Questão 1 - CC", "F" },
+                        { "Questão 1 - DD", "V" },
+                    };
+
                     do {
                         scan.nextLine();
                         System.out.print("\nNome do usuário: ");
@@ -35,7 +42,43 @@ public class question3 {
 
                     System.out.println("\nVamos começar o quiz então " + nameUser + "!");
 
+                    for (int i = 0; i < arrayOfQuestionsAndAnswers.length; i++) {
+                        String resposta;
+
+                        System.out.println("\nQuestão " + (i + 1));
+                        System.out.println(arrayOfQuestionsAndAnswers[i][0]);
+
+                        do {
+                            System.out.printf("Resposta - Questão %d: ", i + 1);
+                            resposta = scan.nextLine();
+
+                            // Tirando os espaços em branco das extremidades
+                            resposta = resposta.trim();
+
+                            if (resposta == null || resposta == "") {
+                                System.out.println("\nPor favor, digite uma resposta!\n");
+                            }
+                        } while (resposta == null || resposta == "");
+
+                        if (resposta.equals(arrayOfQuestionsAndAnswers[i][1])) {
+                            System.out.println("\n****************************************************************\n");
+                            System.out.println("\t\tVOCÊ ACERTOU! +1 em acertos");
+                            System.out.println("\n****************************************************************\n");
+                            rights++;
+                        } else {
+                            System.out.println("\n****************************************************************\n");
+                            System.out.println("\t\tVocê ERROU! :( +1 em erros");
+                            System.out.println("\n****************************************************************\n");
+                            wrongs++;
+                        }
+
+                        showInfosPlayer(rights, wrongs, nameUser);
+                    }
+
+                    System.out.println("\n\n\n\n\n###################################################\n");
+                    System.out.println("\t\tRESULTADO FINAL!");
                     showInfosPlayer(rights, wrongs, nameUser);
+                    System.out.println("\n###################################################\n\n");
 
                     break;
                 case 2:
@@ -50,10 +93,8 @@ public class question3 {
     }
 
     public static void showInfosPlayer(int rights, int wrongs, String nameUser) {
-        System.out.println("\n****************************************************************\n");
-        System.out.println("\tUsuário: \t" + nameUser);
-        System.out.println("\tAcertos: \t" + rights);
-        System.out.println("\tErros: \t\t" + wrongs);
-        System.out.println("\n****************************************************************\n");
+        System.out.println("\n\t\tUsuário: \t" + nameUser);
+        System.out.println("\t\tAcertos: \t" + rights);
+        System.out.println("\t\tErros: \t\t" + wrongs);
     }
 }
