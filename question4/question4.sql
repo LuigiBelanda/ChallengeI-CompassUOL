b0d26c9a7c1cb2ebc5b9b9f15e527e79780e1f97
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-05-09 16:19:28 -0300

    Create table Person

--- question4/question4.sql
+++ question4/question4.sql
@@ -10,3 +10,18 @@ CREATE TABLE Address (
 	state 			VARCHAR[100],
 	country 		VARCHAR[100]
 );
+
+CREATE TABLE Person (
+	person_id 		INT UNIQUE PRIMARY KEY,
+	name 			VARCHAR[255],
+	age 			SMALLINT,
+	phone 			VARCHAR[15],
+	height 			FLOAT, 
+	email 			VARCHAR[255],
+	cpf 			VARCHAR[14],
+	bithday 		DATE,
+	address_id 		INT, 
+	
+	FOREIGN KEY (address_id) REFERENCES Address (addressId),
+	CHECK (age > 0)
+);
