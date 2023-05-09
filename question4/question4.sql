20e63606ae435e030d6e50eef7089978294651c3
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-05-09 16:18:59 -0300

    Create table Address

--- question4/question4.sql
+++ question4/question4.sql
@@ -1,2 +1,12 @@
 CREATE DATABASE question4 
 
+CREATE TABLE Address (
+	addressId 		INT UNIQUE PRIMARY KEY,
+	zip 			VARCHAR[10],
+	street 			VARCHAR[100],
+	complement 		VARCHAR[100],
+	neighborhood 	VARCHAR[100],
+	city 			VARCHAR[100],
+	state 			VARCHAR[100],
+	country 		VARCHAR[100]
+);
