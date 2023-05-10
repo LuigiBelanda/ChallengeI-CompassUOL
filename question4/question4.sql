457e2946cfd3bd08eb56bb39549a240d14109acd
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-05-10 15:29:34 -0300

    Fixing VARCHAR type fields, removing { } from INSERT and test scripts

--- question4/question4.sql
+++ question4/question4.sql
@@ -7,24 +7,24 @@ CREATE DATABASE question4
 -- ETAPA 2.1: CREATE TABLE Address
 CREATE TABLE Address (
 	address_id			SERIAL 			UNIQUE PRIMARY KEY,
-	zip_code			VARCHAR[10]		NOT NULL,
-	address 			VARCHAR[100]	NOT NULL,
-	complement 			VARCHAR[100],
-	neighborhood 		VARCHAR[100]	NOT NULL,
-	city 				VARCHAR[100]	NOT NULL,
-	state 				VARCHAR[100]	NOT NULL,
-	country 			VARCHAR[100]	NOT NULL
+	zip_code			VARCHAR(10)		NOT NULL,
+	address 			VARCHAR(100)	NOT NULL,
+	complement 			VARCHAR(100),
+	neighborhood 		VARCHAR(100)	NOT NULL,
+	city 				VARCHAR(100)	NOT NULL,
+	state 				VARCHAR(100)	NOT NULL,
+	country 			VARCHAR(100)	NOT NULL
 );
 
 -- ETAPA 2.2: CREATE TABLE Person
 CREATE TABLE Person (
 	person_id 			SERIAL			UNIQUE PRIMARY KEY,
-	name 				VARCHAR[255]	NOT NULL,
+	name 				VARCHAR(255)	NOT NULL,
 	age 				SMALLINT		NOT NULL,
-	phone 				VARCHAR[15] 	NOT NULL UNIQUE,
+	phone 				VARCHAR(15) 	NOT NULL UNIQUE,
 	height 				REAL			NOT NULL, 
-	email 				VARCHAR[255]	NOT NULL UNIQUE,
-	CPF 				VARCHAR[14]		NOT NULL UNIQUE,
+	email 				VARCHAR(255)	NOT NULL UNIQUE,
+	CPF 				VARCHAR(14)		NOT NULL UNIQUE,
 	date_of_birthday 	DATE			NOT NULL,
 	id_address 			INT				NOT NULL, 
 	
--- question4/question4_tests.sql
+++ question4/question4_tests.sql
@@ -1,25 +1,25 @@
 -- ETAPA 4: Tests
 -- ETAPA 4.1: Test check age
 INSERT INTO Person (name, age, phone, height, email, CPF, date_of_birthday, id_address) VALUES
-	('{Luan Cristovão}', 0, '{(44) 11111-1111}', 1.55, '{luan.cristovão@gmail.com}', '{111.111.111-11}', '2023-08-16', 1)
+	('Luan Cristovão', 0, '(44) 11111-1111', 1.55, 'luan.cristovão@gmail.com', '111.111.111-11', '2023-08-16', 1)
 
 
 
 -- ETAPA 4.2: UNIQUES (Person)
 -- ETAPA 4.2.1: phone
 INSERT INTO Person (name, age, phone, height, email, CPF, date_of_birthday, id_address) VALUES
-	('{Luan Cristovão}', 18, '{(16) 76678-8585}', 1.55, '{luan.cristovão@gmail.com}', '{111.111.111-11}', '2023-08-16', 1);
+	('Luan Cristovão', 18, '(16) 76678-8585', 1.55, 'luan.cristovão@gmail.com', '111.111.111-11', '2023-08-16', 1);
 
 -- ETAPA 4.2.2: email
 INSERT INTO Person (name, age, phone, height, email, CPF, date_of_birthday, id_address) VALUES
-	('{Luan Cristovão}', 18, '{(44) 11111-1111}', 1.55, '{cleber.machado@gmail.com}', '{111.111.111-11}', '2023-08-16', 1);
+	('Luan Cristovão', 18, '(44) 11111-1111', 1.55, 'cleber.machado@gmail.com', '111.111.111-11', '2023-08-16', 1);
 
 -- ETAPA 4.2.3: CPF
 INSERT INTO Person (name, age, phone, height, email, CPF, date_of_birthday, id_address) VALUES
-	('{Luan Cristovão}', 18, '{(44) 11111-1111}', 1.55, '{luan.cristovão@gmail.com}', '{987.654.321-11}', '2023-08-16', 1);
+	('Luan Cristovão', 18, '(44) 11111-1111', 1.55, 'luan.cristovão@gmail.com', '987.654.321-11', '2023-08-16', 1);
 
 
 
 -- ETAPA 4.3: Verify FK 
 INSERT INTO Person (name, age, phone, height, email, CPF, date_of_birthday, id_address) VALUES
-	('{Luan Cristovão}', 18, '{(44) 11111-1111}', 1.55, '{luan.cristovão@gmail.com}', '{111.111.111-11}', '2023-08-16', 3);
+	('Luan Cristovão', 18, '(44) 11111-1111', 1.55, 'luan.cristovão@gmail.com', '111.111.111-11', '2023-08-16', 3);
