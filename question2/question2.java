c1164dcf75853eb5ed8a51a08e8c86289fef2435
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-05-10 14:17:32 -0300

    Adding logic to not let the program continue until the user types the message

--- question2/question2.java
+++ question2/question2.java
@@ -7,16 +7,25 @@ public class question2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite a mensagem para saber o sentimento que ela expressa: \n");
-        System.out.println("Utilize os caracteres :-) ou :-( \n");
-        String message = scanner.nextLine();
+        System.out.println("\n####### QUESTION 2 #######");
 
-        System.out.println(message);
+        String message;
 
-        message = sanitizeString(message);
+        do {
+            System.out.println("\n\nDigite a mensagem para saber o sentimento que ela expressa:");
+            System.out.println("Utilize os caracteres :-) ou :-( para sabermos seus sentimentos");
+            System.out.print("\nSua mensagem: ");
+            message = scanner.nextLine();
 
-        System.out.println(message);
+            message = sanitizeString(message);
 
+            if (message == null || message == "") {
+                System.out.println("\n\n************************************\n");
+                System.out.println("Por favor, digite alguma mensagem!");
+                System.out.println("\n************************************");
+            }
+        } while (message == null || message == "");
+        
         scanner.close();
     }
 
